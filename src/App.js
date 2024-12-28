4a906e6379239a8d6ab78e4c0c1bbf1a57c3574a
Venkat <venkatasubbareddykattameedi@my.unt.edu>
2024-12-28 09:41:00 -0600

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react"; 
+import axios from "axios"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+  const [users, setUsers] = useState([])
+
+  useEffect(()=>{
+    fetchUsers()
+  }, [])
+
+  const fetchUsers= async ()=>{
+    try {
+      const usersData = await axios.get("localhost:3001/users")
+      console.log("Usersdata:", JSON.stringify(usersData))
+    //  if(usersData.data)
+    } catch (error) {
+      console.log("fetchUsers Error:", error)
+    }
+  }
+
+  return (<div>
+    <header> 
+      <h1> 
+        users:
+      </h1>
+    </header>
+  </div>)
 }
 
-export default App;
+//module.exports = {App}
+export default App
